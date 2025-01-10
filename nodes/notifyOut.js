07f9f0594b1e49bc4022c7299c86b430e44b3031
apxc <alessio.pellizzaro@innotech.email>
2025-01-10 17:27:17 +0100

    BugFix Error on Not set correct configuration

--- nodes/notifyOut.js
+++ nodes/notifyOut.js
@@ -11,17 +11,47 @@ module.exports = function(RED) {
 
         node.on('input', async function(msg) {
             if(msg.hasOwnProperty("notify")) {
-                node.log(`Type: ${node.notifyConfig.options.typenotify}`)
-                switch(node.notifyConfig.options.typenotify) {
-                    case "AadaptiveCards": 
-                        let testValid =  validateBySchema(msg.notify);
-                        if(testValid) {
-                            try{
+                try {
+                    switch(node.notifyConfig.options.typenotify) {
+                        case "AadaptiveCards": 
+                            let testValid =  validateBySchema(msg.notify);
+                            if(testValid) {
+                                try{
+                                    let options = {
+                                        method: 'POST',
+                                        url: node.notifyConfig.options.url,
+                                        rejectUnauthorized: false,
+                                        data: generateAdaptiveCard(msg.notify),
+                                        headers: {
+                                        'Content-Type': 'application/json',
+                                        },
+                                    };
+                                    await axios(options).then(() => {
+                                        node.status({ fill: 'green', shape: 'dot', text: 'Success Send Notify' });
+                                        node.send(msg);
+                                    });
+                                }
+                                catch (e) {
+                                    node.error(e, msg);
+                                    node.status({ fill: 'red', shape: 'dot', text: 'Error Generic' });
+                                }
+                            }
+                            else {
+                                node.error('Not Valid Notify Object', msg);
+                                node.status({ fill: 'red', shape: 'dot', text: 'Not Valid Notify Object' });
+                            }
+                            break;
+                        case "Custom":
+                            if(!msg.notify) {
+                                node.error('Not Valid Notify Object', msg);
+                                node.status({ fill: 'red', shape: 'dot', text: 'Not Valid Notify Object' });
+                            }
+                            else {
                                 let options = {
                                     method: 'POST',
                                     url: node.notifyConfig.options.url,
                                     rejectUnauthorized: false,
-                                    data: generateAdaptiveCard(msg.notify),
+                                    data: msg.notify,
                                     headers: {
                                     'Content-Type': 'application/json',
                                     },
@@ -31,41 +61,17 @@ module.exports = function(RED) {
                                     node.send(msg);
                                 });
                             }
-                            catch (e) {
-                                node.error(e, msg);
-                                node.status({ fill: 'red', shape: 'dot', text: 'Error Generic' });
-                            }
-                        }
-                        else {
-                            node.error('Not Valid Notify Object', msg);
-                            node.status({ fill: 'red', shape: 'dot', text: 'Not Valid Notify Object' });
-                        }
-                        break;
-                    case "Custom":
-                        if(!msg.notify) {
-                            node.error('Not Valid Notify Object', msg);
-                            node.status({ fill: 'red', shape: 'dot', text: 'Not Valid Notify Object' });
-                        }
-                        else {
-                            let options = {
-                                method: 'POST',
-                                url: node.notifyConfig.options.url,
-                                rejectUnauthorized: false,
-                                data: msg.notify,
-                                headers: {
-                                'Content-Type': 'application/json',
-                                },
-                            };
-                            await axios(options).then(() => {
-                                node.status({ fill: 'green', shape: 'dot', text: 'Success Send Notify' });
-                                node.send(msg);
-                            });
-                        }
-                        break;
-                    default:
-                        node.error('Not Manage Type Notify', msg);
-                        node.status({ fill: 'red', shape: 'dot', text: 'Not Manage Type Notify' });
+                            break;
+                        default:
+                            node.error('Not Manage Type Notify', msg);
+                            node.status({ fill: 'red', shape: 'dot', text: 'Not Manage Type Notify' });
+                    }
                 }
+                catch (e) {
+                    node.error(`Error on Configuration \n Not Set a Correct Configuration \n ${e}`, msg);
+                    node.status({ fill: 'red', shape: 'dot', text: 'Error on Configuration' });
+                }
+
             }
             else {
                 node.error('Not set a msg.notify props', msg);
